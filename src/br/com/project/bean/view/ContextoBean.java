c945600ded3a9a7e73dce87689141092cc676250
JonilsonNascimento <jonnyzerobravo@gmail.com>
2019-05-10 22:38:36 -0400

    Correcao de erro j_spring_security_check

---
+++ src/br/com/project/bean/view/ContextoBean.java
@@ -0,0 +1,84 @@
+package br.com.project.bean.view;
+
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+
+import br.com.project.model.classes.Entidade;
+
+@Scope(value = "session")
+@Component(value = "contextoBean")
+public class ContextoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String USER_LOGADO_SESSAO = "userLogadoSessao";
+	
+
+
+	/**
+	 * 
+	 * @return String Usuário logado remote user
+	 */
+	public String getUserLogado() {
+		return getExternalContext().getRemoteUser();
+	}
+
+	/**
+	 * 
+	 * @return String Usuário logado user principal
+	 */
+	public String getUserPrincipal() {
+		return getExternalContext().getUserPrincipal().getName();
+	}
+
+	/**
+	 * 
+	 * @return String Usuário logado Authentication Spring security
+	 */
+	public Authentication getAuthentication() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+
+	/**
+	 * 
+	 * @return ExternalContext da application
+	 */
+	public ExternalContext getExternalContext() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext external = context.getExternalContext();
+		return external;
+	}
+
+	/**
+	 * @return Entidade logado na sessão atualmente
+	 */
+	
+	
+	/**
+	 * Retorna se o usuario logado possui os acesso passados como paramentro, este metodo destina-se ao uso onde não é possivel usar
+	 * as tags do spring security
+	 */
+	public boolean possuiAcesso(String... acessos) {
+		for (String acesso : acessos) {
+			for (GrantedAuthority autorizacao : getAuthentication().getAuthorities()){ 
+				if (autorizacao.getAuthority().trim().equals(acesso.trim())){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+}
--- src/br/com/project/model/classes/Entidade.java
+++ src/br/com/project/model/classes/Entidade.java
@@ -3,6 +3,8 @@ package br.com.project.model.classes;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.envers.Audited;
@@ -15,6 +17,7 @@ public class Entidade implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long ent_codigo;
 	
 	private String ent_login = null;
@@ -22,6 +25,7 @@ public class Entidade implements Serializable{
 	private String ent_senha;
 	
 	private boolean ent_inativo;
+	
 
 	public Long getEnt_codigo() {
 		return ent_codigo;
